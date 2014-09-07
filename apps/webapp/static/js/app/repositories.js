92da822cc50f7daff2f60b897b11e56e6fbb4560
James Hannington <jam.hann@gmail.com>
2014-09-07 18:30:20 +0100

    Ensure API calls have a trailing slash.

--- apps/webapp/static/js/app/repositories.js
+++ apps/webapp/static/js/app/repositories.js
@@ -1,20 +1,20 @@
 var module = angular.module('repositories', []);
 
 module.factory('EventRepository', function($http) {
-    var baseUrl = '/api/events';
+    var baseUrl = '/api/events/';
     
     return {
         get: function(id) {
-            return $http.get(baseUrl+'/'+id);
+            return $http.get(baseUrl+id+'/');
         },
         list: function(params) {
             return $http.get(baseUrl, { params: params });
         },
         create: function(data) {
-            return $http.post(baseUrl+'/', data);
+            return $http.post(baseUrl, data);
         },
         addAttendee: function(id, userId) {
-            return $http.post(baseUrl+'/'+id+'/add_attendee/', {
+            return $http.post(baseUrl+id+'/add_attendee/', {
                 'user_id': userId
             });
         }
@@ -22,32 +22,32 @@ module.factory('EventRepository', function($http) {
 });
 
 module.factory('FileRepository', function($http) {
-    var baseUrl = '/api/files';
+    var baseUrl = '/api/files/';
     
     return {
         get: function(id) {
-            return $http.get(baseUrl+'/'+id);
+            return $http.get(baseUrl+id+'/');
         },
         delete: function(id) {
-            return $http.delete(baseUrl+'/'+id);
+            return $http.delete(baseUrl+id+'/');
         },
         createSubdirectory: function(id, name) {
-            return $http.post(baseUrl+'/'+id+'/create_subdirectory/', {
+            return $http.post(baseUrl+id+'/create_subdirectory/', {
                 'name': name
             });
         },
         getFileShare: function(id) {
-            return $http.get(baseUrl+'/'+id+'/share');
+            return $http.get(baseUrl+id+'/share/');
         }
     };
 });
 
 module.factory('MilestoneRepository', function($http) {
-    var baseUrl = '/api/milestones';
+    var baseUrl = '/api/milestones/';
     
     return {
         get: function(id) {
-            return $http.get(baseUrl+'/'+id);
+            return $http.get(baseUrl+id+'/');
         },
         list: function(params) {
             return $http.get(baseUrl, { params: params });
@@ -56,55 +56,55 @@ module.factory('MilestoneRepository', function($http) {
 });
 
 module.factory('TaskRepository', function($http) {
-    var baseUrl = '/api/tasks';
+    var baseUrl = '/api/tasks/';
     
     return {
         get: function(id) {
-            return $http.get(baseUrl+'/'+id);
+            return $http.get(baseUrl+id+'/');
         },
         list: function() {
             return $http.get(baseUrl);
         },
         create: function(data) {
-            return $http.post(baseUrl+'/', data);
+            return $http.post(baseUrl, data);
         },
         update: function(id, data) {
-            return $http.put(baseUrl+'/'+id, data);
+            return $http.put(baseUrl+id+'/', data);
         },
         delete: function(id) {
-            return $http.delete(baseUrl+'/'+id);
+            return $http.delete(baseUrl+id+'/');
         }
     };
 });
 
 module.factory('TaskForceRepository', function($http) {
-    var baseUrl = '/api/taskforces';
+    var baseUrl = '/api/taskforces/';
     
     return {
         get: function(id) {
-            return $http.get(baseUrl+'/'+id);
+            return $http.get(baseUrl+id+'/');
         },
         list: function(params) {
             return $http.get(baseUrl, { params: params });
         },
         create: function(data) {
-            return $http.post(baseUrl+'/', data);
+            return $http.post(baseUrl, data);
         },
         update: function(id, data) {
-            return $http.put(baseUrl+'/'+id, data);
+            return $http.put(baseUrl+id+'/', data);
         },
         delete: function(id) {
-            return $http.delete(baseUrl+'/'+id);
+            return $http.delete(baseUrl+id+'/');
         }
     };
 });
 
 module.factory('TeamRepository', function($http) {
-    var baseUrl = '/api/teams';
+    var baseUrl = '/api/teams/';
     
     return {
         get: function(id) {
-            return $http.get(baseUrl+'/'+id);
+            return $http.get(baseUrl+id+'/');
         },
         list: function() {
             return $http.get(baseUrl);
@@ -113,21 +113,21 @@ module.factory('TeamRepository', function($http) {
 });
 
 module.factory('UserRepository', function($http) {
-    var baseUrl = '/api/users';
-    var profilesUrl = '/api/user-profiles';
+    var baseUrl = '/api/users/';
+    var profilesUrl = '/api/user-profiles/';
     
     return {
         get: function(id) {
-            return $http.get(baseUrl+'/'+id);
+            return $http.get(baseUrl+id+'/');
         },
         getCurrentUser: function() {
-            return $http.get(baseUrl+'/?current');
+            return $http.get(baseUrl+'?current');
         },
         list: function(params) {
             return $http.get(baseUrl, { params: params });
         },
         updateProfile: function(id, data) {
-            return $http.put(profilesUrl+'/'+id, data);
+            return $http.put(profilesUrl+id+'/', data);
         }
     };
 });
