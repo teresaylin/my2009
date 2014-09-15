326e5dab1daf6c9d5e793976cccf6cf6d6264b4d
James Hannington <jam.hann@gmail.com>
2014-09-15 18:41:21 +0100

    Added change password and reset password functionality.

--- apps/webapp/urls.py
+++ apps/webapp/urls.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django.conf.urls import patterns, include, url
 import django.contrib.auth.views as authViews
 
@@ -12,6 +13,25 @@ urlpatterns = patterns('',
     url(r'^$', AppView.as_view(), name="app"),
 
     # Authentication
-    url(r'^login$', authViews.login, {'template_name': 'webapp/login.html' }, name="login"),
-    url(r'^logout$', authViews.logout_then_login, name="logout"),
+    url(r'^login$', authViews.login, {'template_name': 'webapp/login.html' }, name='login'),
+    url(r'^logout$', authViews.logout_then_login, name='logout'),
+    url(r'^change-password$', authViews.password_change, {
+            'template_name': 'webapp/change-password.html',
+            'post_change_redirect': 'webapp:change-password-done',
+            'current_app': 'webapp'
+        }, name='change-password'
+    ),
+    url(r'^change-password-done$', authViews.password_change_done, {
+            'template_name': 'webapp/change-password-done.html',
+        }, name='change-password-done'
+    ),
+    url(r'reset-password$', authViews.password_reset, {
+            'template_name': 'webapp/reset-password.html',
+            'post_reset_redirect': 'webapp:reset-password-done',
+        }, name='reset-password'
+    ),
+    url(r'^reset-password-done$', authViews.password_reset_done, {
+            'template_name': 'webapp/reset-password-done.html',
+        }, name='reset-password-done'
+    ),
 )
