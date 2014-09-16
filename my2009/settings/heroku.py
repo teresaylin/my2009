4dee587e7804af4478254447636a615583a9c4de
James Hannington <jam.hann@gmail.com>
2014-09-16 19:25:20 +0100

    Fixed relative STATIC_ROOT path

--- my2009/settings/heroku.py
+++ my2009/settings/heroku.py
@@ -15,5 +15,5 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
