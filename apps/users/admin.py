040a61b709d2a07df2947883df9970328a192952
James Hannington <jam.hann@gmail.com>
2014-09-17 16:45:07 +0100

    Added "Send welcome e-mail" action to user admin page

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
+from django.template import Context
+from django.template.loader import get_template
+from django.core.mail import send_mass_mail
+from django.core.urlresolvers import reverse
+from django.contrib.messages import ERROR, WARNING
 from apps.users.models import Team, UserTeamMapping, Role, UserRoleMapping, UserProfile, Milestone, TaskForce, UserTaskForceMapping
 
 class TeamAdmin(admin.ModelAdmin):
@@ -54,3 +59,44 @@ class UserTaskForceMappingAdmin(admin.ModelAdmin):
     list_display = ('user', 'task_force')
 
 admin.site.register(UserTaskForceMapping, UserTaskForceMappingAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    actions = ['sendWelcomeEmail']
+
+    def sendWelcomeEmail(self, request, queryset):
+        """Generates a random password for, and sends a welcome e-mail to, the selected users"""
+        messages = []
+        subject = get_template('users/email/welcome-email-subject.txt').render(Context({}))
+        appUrl = request.build_absolute_uri(reverse('webapp:app'))
+
+        for user in queryset:
+            # Check user has an e-mail address
+            if user.email == '':
+                self.message_user(request, 'User %s has no e-mail address' % user.username, ERROR)
+                continue
+
+            # Assign new password
+            newPassword = User.objects.make_random_password()
+            user.set_password(newPassword)
+            user.save()
+
+            # Prepare e-mail
+            context = {
+                'appUrl': appUrl,
+                'user': user,
+                'password': newPassword
+            }
+            message = get_template('users/email/welcome-email.txt').render(Context(context))
+            messages.append((subject, message, None, [user.email]))
+        
+        # Send messages
+        if len(messages) > 0:
+            sent = send_mass_mail(messages, fail_silently=True)
+            self.message_user(request, '%d e-mails sent' % sent)
+        else:
+            self.message_user(request, 'No e-mails sent', WARNING)
+
+    sendWelcomeEmail.short_description = 'Send welcome e-mail'
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
