f9b3a6c385a2d51768ee7f4c4111f85a6daff528
James Hannington <jam.hann@gmail.com>
2015-09-07 17:41:56 +0100

    Added missing file

---
+++ apps/stats/config.py
@@ -0,0 +1,21 @@
+from django.apps import AppConfig
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Permission
+
+class StatsConfig(AppConfig):
+    name = 'apps.stats'
+
+    def ready(self):
+        # Create generic "view stats" permission
+
+        ct, created = ContentType.objects.get_or_create(
+            name='stats',
+            app_label='stats',
+            model='none'
+        )
+
+        Permission.objects.get_or_create(
+            name='Can view stats',
+            content_type=ct,
+            codename='can_view_stats'
+        )
