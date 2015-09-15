6434ba18a67610611a80ba8787c4513975456bc6
James Hannington <jam.hann@gmail.com>
2015-09-15 12:50:40 +0100

    Added group assign/unassign functionality to admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.admin import UserAdmin
 from django.template import Context
 from django.template.loader import get_template
@@ -71,13 +71,14 @@ from django.contrib import messages
 from django.db import IntegrityError
 
 class CustomUserAdmin(UserAdmin):
-    actions = ['sendWelcomeEmail', 'assignRole']
+    actions = ['sendWelcomeEmail', 'assignRole', 'assignGroup']
 
     def get_urls(self):
         # Add "assign role" view to available URLs
         urls = super().get_urls()
         return patterns('',
             url(r'^assign_role/$', self.__class__.AssignRoleView.as_view(), name='assign-role'),
+            url(r'^assign_group/$', self.__class__.AssignGroupView.as_view(), name='assign-group'),
         ) + urls
 
     def sendWelcomeEmail(self, request, queryset):
@@ -164,8 +165,65 @@ class CustomUserAdmin(UserAdmin):
             'form': form,
             'users': queryset
         }, context_instance=RequestContext(request))
-        
     assignRole.short_description = 'Assign role'
+
+    class AssignGroupForm(forms.Form):
+        users = forms.ModelMultipleChoiceField(queryset=User.objects.all())
+        group = forms.ModelChoiceField(queryset=Group.objects.all())
+        unassign = forms.BooleanField(required=False, label='Remove users from group?')
+    
+    class AssignGroupView(TemplateView):
+        template_name = 'users/admin/assign-group.html'
+        
+        def post(self, request):
+            form = CustomUserAdmin.AssignGroupForm(request.POST)
+            
+            if form.is_valid():
+                users = form.cleaned_data['users']
+                group = form.cleaned_data['group']
+                
+                if form.cleaned_data['unassign']:
+                    # Unassign group
+                    count = 0
+                    for user in users:
+                        user.groups.remove(group)
+                        count += 1
+                    
+                    # Send user message and redirect to user list
+                    if count > 0:
+                        messages.add_message(request, messages.INFO, '%d users removed from %s' % (count, group.name))
+                    else:
+                        messages.add_message(request, messages.WARNING, 'No users were removed from %s' % (group.name,))
+                else:
+                    # Assign group
+                    count = 0
+                    for user in users:
+                        user.groups.add(group)
+                        count += 1
+                    
+                    # Send user message and redirect to user list
+                    if count > 0:
+                        messages.add_message(request, messages.INFO, '%d users assigned to %s' % (count, group.name))
+                    else:
+                        messages.add_message(request, messages.WARNING, 'No users were assigned to %s' % (group.name,))
+
+                return redirect('admin:auth_user_changelist')
+
+            context = super().get_context_data()
+            context['form'] = form
+            return self.render_to_response(context)
+
+    def assignGroup(self, request, queryset):
+        """Assign selected users to a group"""
+        form = self.__class__.AssignGroupForm(initial={
+            'users': queryset
+        })
+        return render_to_response('users/admin/assign-group.html', {
+            'form': form,
+            'users': queryset
+        }, context_instance=RequestContext(request))
+    assignGroup.short_description = 'Assign/unassign group'
+
 
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
